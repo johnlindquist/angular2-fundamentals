3022a061ceed2f57b32af5f799e0207f7d6c14b7
John Lindquist <johnlindquist@gmail.com>
2016-09-27 16:25:21 -0600

    adding events-and-refs

--- src/app/simple-form/simple-form.component.ts
+++ src/app/simple-form/simple-form.component.ts
@@ -2,14 +2,19 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-simple-form',
-  template: `
-    <p>
-      simple-form Works!
-    </p>
-  `,
+  template: `<div>
+
+<input #myInput type="text">
+<button (click)="onClick(myInput.value)">Click me!</button>
+
+</div>`,
   styles: []
 })
 export class SimpleFormComponent implements OnInit {
+
+  onClick(value){
+    console.log(value);
+  }
 
   constructor() { }
 
