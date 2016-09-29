e0c3d2e55699f5c1ff2c4aa6fd9609f9dad171aa
John Lindquist <johnlindquist@gmail.com>
2016-09-29 13:06:37 -0600

    adding style-encapsulation

--- src/app/simple-form/simple-form.component.ts
+++ src/app/simple-form/simple-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-simple-form',
@@ -12,7 +12,9 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
   (mouseup)="isMousedown = false"
   (mouseleave)="isMousedown = false"
   >
-<button (click)="update.emit({text:message})">Click me!</button>
+<button
+ class="white bg-black code"
+ (click)="update.emit({text:message})">Click me!</button>
 `,
   styles: [`
 :host{
